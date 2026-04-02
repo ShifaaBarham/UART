22adfda8d7e918456c642e013d2353d775d58550
Shifaa Barham <barhamshifaa@gmail.com>
2026-04-03 01:56:26 +0300

    Refactor UART_Agent class to remove virtual interface parameter from constructor and streamline monitor initialization.

--- tb/agents/uart_agent/uart_Agent.sv
+++ tb/agents/uart_agent/uart_Agent.sv
@@ -5,24 +5,23 @@ class UART_Agent #(parameter DATA_WIDTH=8) ;
   uart_Monitor #( DATA_WIDTH) mon;
   uart_Agent_config #( DATA_WIDTH) agent_config;
   
-  virtual interface UART_if #(DATA_WIDTH) vif;
+
   mailbox #(uart_Transaction #( DATA_WIDTH)) uart_drv_mbx;
   mailbox #(uart_Transaction #( DATA_WIDTH)) uart_mon_mbx;
   
-  function new(virtual interface UART_if #(DATA_WIDTH) vif,
+  function new(
                uart_Agent_config #( DATA_WIDTH) agent_config,
                mailbox #(uart_Transaction #( DATA_WIDTH)) uart_drv_mbx, 
                mailbox #(uart_Transaction #( DATA_WIDTH)) uart_mon_mbx );
        
-        this.vif = vif;
         this.agent_config=agent_config;
         this.uart_drv_mbx=uart_drv_mbx;
         this.uart_mon_mbx=uart_mon_mbx;
 
-    mon=new(vif,uart_mon_mbx,agent_config);
+    mon=new(agent_config.vif,uart_mon_mbx,agent_config);
 
         if(agent_config.is_active == 1)
-          drv=new(vif , uart_drv_mbx,agent_config);    
+          drv=new(agent_config.vif , uart_drv_mbx,agent_config);    
   endfunction 
       
       task run();
