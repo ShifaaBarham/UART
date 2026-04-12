8f53e7efd1f6b485e00ad73cc09b9d8bf3bd8250
Shifaa Barham <barhamshifaa@gmail.com>
2026-04-12 15:26:26 +0300

    Refactor constructor parameters in test_config_read_backpressure class for consistency; update types for clarity and improved functionality.

--- tb/tests/3_test_config_read_backpressure.sv
+++ tb/tests/3_test_config_read_backpressure.sv
@@ -1,10 +1,10 @@
 class test_config_read_backpressure extends base_test;
 
-  function new( virtual valid_ready_if v_cfg,
-                virtual valid_ready_if v_tx,
-                virtual valid_ready_if v_rx,
-                virtual UART_if        u_tx,
-                virtual UART_if        u_rx );
+  function new( virtual valid_ready_if #(8,8,1)  v_cfg,
+                virtual valid_ready_if #(32,8,1) v_tx,
+                virtual valid_ready_if #(32,8,1) v_rx,
+                virtual UART_if        #(8)      u_tx,
+                virtual UART_if        #(8)      u_rx );
     super.new(v_cfg, v_tx, v_rx, u_tx, u_rx);
   endfunction
 
@@ -12,6 +12,10 @@ class test_config_read_backpressure extends base_test;
     $display("[%0t] [TEST-3] Running: test_config_read_backpressure", $time);
     $display("=================================================");
 
+
+      write_register(8'h14, 8'hFF);
+    write_register(8'h24, 8'hFF); 
+    
     read_register(8'h14, 15); 
     
     read_register(8'h24, $urandom_range(5, 20));
