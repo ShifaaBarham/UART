2881e1fb9dede8c96298b722ea38f18dfcac661c
Shifaa Barham <barhamshifaa@gmail.com>
2026-05-04 13:28:54 +0300

    Update base_test class to support 32-bit configurations; enhance watchdog functionality and improve reset logic for better simulation reliability.

--- tb/tests/0_base_test.sv
+++ tb/tests/0_base_test.sv
@@ -1,33 +1,34 @@
 class base_test;
   uart_env env;
-
+  time watchdog_limit = 500ms;
+  
   vr_Agent_config #(8,8,1)   cfg_config;  
   vr_Agent_config #(32,8,1)  cfg_vr_tx; 
   vr_Agent_config #(32,8,1)  cfg_vr_rx;  
-  uart_Agent_config #(8)     cfg_uart_tx;
-  uart_Agent_config #(8)     cfg_uart_rx;
+  uart_Agent_config #(32)     cfg_uart_tx;
+  uart_Agent_config #(32)     cfg_uart_rx;
 
   virtual valid_ready_if #(8,8,1)  vif_config; 
   virtual valid_ready_if #(32,8,1) vif_vr_tx;  
   virtual valid_ready_if #(32,8,1) vif_vr_rx;
-  virtual UART_if        #(8)      vif_uart_tx;
-  virtual UART_if        #(8)      vif_uart_rx;
+  virtual UART_if        #(32)      vif_uart_tx;
+  virtual UART_if        #(32)      vif_uart_rx;
 
 mailbox #(vr_Transaction #(8,8,1))  mbx_gen2drv_config;
   mailbox #(vr_Transaction #(32,8,1)) mbx_gen2drv_tx;
   mailbox #(vr_Transaction #(32,8,1)) mbx_gen2drv_rx;
-  mailbox #(uart_Transaction #(8))    mbx_gen2drv_uart_rx;
+  mailbox #(uart_Transaction #(32))    mbx_gen2drv_uart_rx;
 
   vr_Generator #(8,8,1)   gen_config;
   vr_Generator #(32,8,1)  gen_vr_tx;
   vr_Generator #(32,8,1)  gen_vr_rx;
-  uart_Generator #(8)     gen_uart_rx;
+  uart_Generator #(32)     gen_uart_rx;
 
   function new( virtual valid_ready_if #(8,8,1)  v_cfg,
                 virtual valid_ready_if #(32,8,1) v_tx,
                 virtual valid_ready_if #(32,8,1) v_rx,
-                virtual UART_if        #(8)      u_tx,
-                virtual UART_if        #(8)      u_rx );
+                virtual UART_if        #(32)      u_tx,
+                virtual UART_if        #(32)      u_rx );
     
     this.vif_config = v_cfg; this.vif_vr_tx = v_tx; this.vif_vr_rx = v_rx;
     this.vif_uart_tx = u_tx; this.vif_uart_rx = u_rx;
@@ -43,11 +44,11 @@ mailbox #(vr_Transaction #(8,8,1))  mbx_gen2drv_config;
     gen_uart_rx = new(mbx_gen2drv_uart_rx);
 
     
-   cfg_config  = new(); cfg_config.vif  = vif_config;  cfg_config.agent_type = vr_agent_pkg::MASTER;
+    cfg_config  = new(); cfg_config.vif  = vif_config;  cfg_config.agent_type = vr_agent_pkg::MASTER;
     cfg_vr_tx   = new(); cfg_vr_tx.vif   = vif_vr_tx;   cfg_vr_tx.agent_type  = vr_agent_pkg::MASTER;
     cfg_vr_rx   = new(); cfg_vr_rx.vif   = vif_vr_rx;   cfg_vr_rx.agent_type  = vr_agent_pkg::SLAVE;
-     cfg_uart_tx = new(); cfg_uart_tx.vif = vif_uart_tx; cfg_uart_tx.is_active = 0;
-    cfg_uart_rx = new(); cfg_uart_rx.vif = vif_uart_rx;  cfg_uart_rx.is_active=1;
+    cfg_uart_tx = new(); cfg_uart_tx.vif = vif_uart_tx; cfg_uart_tx.is_active = 0; cfg_uart_tx.is_tx = 1; cfg_uart_tx.agent_type = uart_agent_pkg::SLAVE;
+    cfg_uart_rx = new(); cfg_uart_rx.vif = vif_uart_rx; cfg_uart_rx.is_active = 1; cfg_uart_rx.is_tx = 0; cfg_uart_rx.agent_type = uart_agent_pkg::MASTER;
 
     env = new(cfg_config, cfg_vr_tx, cfg_vr_rx, cfg_uart_tx, cfg_uart_rx,
               mbx_gen2drv_config, mbx_gen2drv_tx, mbx_gen2drv_rx, mbx_gen2drv_uart_rx);
@@ -55,14 +56,31 @@ mailbox #(vr_Transaction #(8,8,1))  mbx_gen2drv_config;
 
   virtual task run();
     env.build();
-    fork env.run(); join_none
-    
-    apply_hw_reset();    
-    configure_dut();     
-    lift_soft_reset();  
-    main_test();        
-    
-    #5000;
+    fork 
+       env.run();
+      
+      join_none
+
+  fork
+
+    begin 
+     apply_hw_reset();    
+     configure_dut();     
+     lift_soft_reset(); 
+     main_test();  
+     end
+     begin
+       repeat(watchdog_limit) #1;
+                $display("=================================================");
+
+        $display("[%0t] [FATAL] WATCHDOG TIMEOUT! Test hung.", $time);
+        $display("=================================================");
+        $fatal(1, "Simulation killed by Watchdog!");
+      end  
+    join_any
+    disable fork;
+
+    #5000000;
     $display("[%0t] [TEST] Simulation Finished.", $time);
     $finish;
   endtask
@@ -77,10 +95,10 @@ mailbox #(vr_Transaction #(8,8,1))  mbx_gen2drv_config;
   endtask
 
   virtual task apply_hw_reset();
-    $display("[%0t] [TEST] Waiting for HW Reset...", $time);
+    $display("[%0t] [TEST] Waiting for HW Reset.", $time);
     wait(vif_config.rst == 0); 
     wait(vif_config.rst == 1); 
-    #50;
+    #5000;
   endtask
 
   virtual task configure_dut();
@@ -89,7 +107,7 @@ mailbox #(vr_Transaction #(8,8,1))  mbx_gen2drv_config;
   virtual task lift_soft_reset();
     $display("[%0t] [TEST] Lifting Soft Reset.", $time);
     write_register(8'h04, 8'h01); 
-    #50;
+    #50000;
   endtask
 
   virtual task main_test();
